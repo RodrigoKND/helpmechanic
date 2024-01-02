978cd23f3e27333570a815325c6edf800c7ef303
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2024-01-02 16:32:47 -0400

    agregando pequeños cambios en el menu de navegacion

--- src/components/HOME/businessCharacteristics/businessCharacteristics.js
+++ src/components/HOME/businessCharacteristics/businessCharacteristics.js
@@ -1,31 +1,33 @@
+import {Link} from 'react-router-dom';
+
 import './businessCharacteristics.css';
 
 export default function BusinessCharacteristics() {
     return (
         <>
             <div className='col-md-4 card border border-light' style={{ background: '#161616' }}>
-                <div className='card-body text-center text-info'>
+                <Link className='card-body text-center text-info' to='/calidad'>
                     <span className="material-symbols-outlined mt-3" style={{ fontSize: '50px' }}>
                         minor_crash
                     </span>
                     <pre className='card__title'>INSPECCIÓN</pre>
-                </div>
+                </Link>
             </div>
             <div className='col-md-4 card border border-light' style={{ background: '#161616' }}>
-                <div className='card-body text-center text-info'>
+                <Link className='card-body text-center text-info' to='/calidad'>
                     <span className="material-symbols-outlined mt-3" style={{ fontSize: '50px' }}>
                         two_wheeler
                     </span>
                     <pre className='card__title'>DIAGNÓSTICO</pre>
-                </div>
+                </Link>
             </div>
             <div className='col-md-4 card border border-light' style={{ background: '#161616' }}>
-                <div className='card-body text-center text-info'>
+                <Link className='card-body text-center text-info' to='/blogHelpMechanic'>
                     <span className="material-symbols-outlined mt-3" style={{ fontSize: '50px' }}>
                         home_repair_service
                     </span>
                     <pre className='card__title'>INFORMACIÓN</pre>
-                </div>
+                </Link>
             </div>
         </>
     );
--- src/components/menu/header.js
+++ src/components/menu/header.js
@@ -1,47 +1,63 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import '../../App.css';
 import '../../fonts.css';
-import './header.css';
 
 export default function Menunav() {
+    const [activeLink, setActiveLink] = useState('');
+
+    const handleClick = (link) => {
+        setActiveLink(link);
+    };
+
     return (
         <header>
-            <nav className='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
-                <div>
-                    <div className='container-fluid bg-primary'
-                        style={{ position: 'fixed', zIndex: '1000', top: '0' }}>
-                        <button className='navbar-toggler mt-4 mb-4' type='button' data-bs-toggle='collapse'
-                            data-bs-target='#navbarNav' aria-controls='navbarNav'
-                            aria-expanded='false' aria-label='Toggle navigation'>
-                            <span className='navbar-toggler-icon'></span>
-                        </button>
-                        <div className='collapse navbar-collapse' id='navbarNav'>
-                            <ul className='navbar-nav menu container title'>
-                                <li className='nav-item me-3 mb-4 mt-2'>
-                                    <Link className='nav-link active border p-2 border border-0 text-white' to='/'>
-                                        Home
-                                    </Link>
-                                </li>
-                                <li className='nav-item me-3 mt-2'>
-                                    <Link className='nav-link active text-white p-2' aria-current='page'
-                                        to='/blogHelpMechanic'>
-                                        Blog
-                                    </Link>
-                                </li>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-primary fixed-top'>
+                <div className='container-fluid bg-primary'
+                    style={{ position: 'fixed', zIndex: '1000', top: '0' }}>
+                    <button className='navbar-toggler mt-4 mb-4' type='button' data-bs-toggle='collapse'
+                        data-bs-target='#navbarNav' aria-controls='navbarNav'
+                        aria-expanded='false' aria-label='Toggle navigation'>
+                        <span className='navbar-toggler-icon'></span>
+                    </button>
+                    <div className='collapse navbar-collapse' id='navbarNav'>
+                        <ul className='navbar-nav menu container title'>
+                            <li className='nav-item me-3 mb-4 mt-2'>
+                                <NavLink
+                                    className={`nav-link border p-2 border border-0 text-white ${activeLink === 'Home' ? 'bg-success' : ''}`}
+                                    to='/' onFocus={() => handleClick('Home')}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className='nav-item me-3 mt-2'>
+                                <NavLink
+                                    onFocus={() => handleClick('Blog')}
+                                    className={`nav-link text-white p-2 ${activeLink === 'Blog' ? 'bg-success' : ''}`}
+                                    aria-current='page'
+                                    to='/blogHelpMechanic'>
+                                    Blog
+                                </NavLink>
+                            </li>
 
-                                <li className='nav-item me-3 mt-2'>
-                                    <Link className='nav-link active text-white p-2' aria-current='page'
-                                        to='/nosotros'>
-                                        Nosotros
-                                    </Link>
-                                </li>
-                                <li className='nav-item  mt-2 me-3 mb-3'>
-                                    <Link className='nav-link active border p-2 border border-0 text-white' to='/calidad'>
-                                        Calidad
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+                            <li className='nav-item me-3 mt-2'>
+                                <NavLink
+                                    onFocus={() => handleClick('Nosotros')}
+                                    className={`nav-link text-white p-2 ${activeLink === 'Nosotros' ? 'bg-success' : ''}`}
+                                    aria-current='page'
+                                    to='/nosotros'>
+                                    Nosotros
+                                </NavLink>
+
+                            </li>
+                            <li className='nav-item  mt-2 me-3 mb-3'>
+                                <NavLink
+                                    onFocus={() => handleClick('Calidad')}
+                                    className={`nav-link border p-2 border border-0 text-white ${activeLink === 'Calidad' ? 'bg-success' : ''}`}
+                                    to='/calidad'>
+                                    Calidad
+                                </NavLink>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
