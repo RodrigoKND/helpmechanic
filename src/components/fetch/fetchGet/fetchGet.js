4e450b71c0654afeb95b518b29e13adc737df42c
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2024-01-17 13:36:18 -0400

    agregando los cambios de errores en el renderizado blanco de la página1

--- src/components/fetch/fetchGet/fetchGet.js
+++ src/components/fetch/fetchGet/fetchGet.js
@@ -1,8 +1,8 @@
 export const fetchGet = async (routeServer) => {
-    const modeDev = 'http://localhost:3001/' + routeServer;
+    // const modeDev = 'http://localhost:3001/' + routeServer;
     const modeProd = 'https://hm-server-provider.onrender.com/' + routeServer;
     return new Promise((resolve) => {
-        fetch(modeDev, {
+        fetch(modeProd, {
             method: 'GET',
             cache: 'no-store',
             credentials: 'include'
