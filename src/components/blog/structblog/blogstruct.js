7636c5ca6fb411431be3648f1b0a6dcf3f1053af
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2024-01-17 09:58:35 -0400

    comprobando errores en el blog1

--- src/components/blog/structblog/blogstruct.js
+++ src/components/blog/structblog/blogstruct.js
@@ -12,21 +12,21 @@ export default function Blogstruct() {
         queryKey: ['blog'],
         queryFn: () => fetchGet('blog')
     })
-    console.log('data: ', query.data)
-    query.data?.map(elements => {
-        console.log(elements)
-        const replaceTitle = elements.title
-            .toLowerCase()
-            .replace(/ /g, '-')
-            .replace(/[^a-z0-9-]/g, '')
-        return replaceTitle === title ? setPost(elements.content) : null
-    })
+    console.log('data: ', query.data?.toLowerCase())
+    // query.data?.map(elements => {
+    //     console.log(elements)
+    //     const replaceTitle = elements.title
+    //         .toLowerCase()
+    //         .replace(/ /g, '-')
+    //         .replace(/[^a-z0-9-]/g, '')
+    //     return replaceTitle === title ? setPost(elements.content) : null
+    // })
     return (
         <main className='text-white' style={{ background: '#1F2730', height: '100%', width: 'auto' }}>
             {
                 query.isLoading && (
                     <>
-                        <h4 className='text-white text-center p-3' style={{ marginTop: '7vh' }}>
+                        <h4 className='text-white text-center p-4' style={{ marginTop: '7vh' }}>
                             Cargando contenido...
                         </h4>
                         <Link to='/' className='nav-link text-center fs-3 p-3 text-primary'>Regresar</Link>
