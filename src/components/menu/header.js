1425e603fb627811b1627f53588d323b55439001
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2024-01-26 19:39:27 -0400

    mejorando el rendimiento de la página para el SEO

--- src/components/menu/header.js
+++ src/components/menu/header.js
@@ -19,7 +19,7 @@ export default function Menunav() {
             animateMenu.current.classList.add('animateMenu');
             menuNav.current.style.display = 'block';
             setCountClick(countClick + 1);
-        }else {
+        } else {
             menuNav.current.style.display = 'none';
             setCountClick(0);
         }
@@ -30,35 +30,36 @@ export default function Menunav() {
             <nav className='navbar navbar-expand-lg navbar-dark bg-primary fixed-top'>
                 <div className='container-fluid bg-primary' ref={animateMenu}
                     style={{ position: 'fixed', zIndex: '1000', top: '0' }}>
-                    <button type='button' className='btn text-white hiddenMenu' onClick={showMenu}>
+                    <button type='button' className='btn text-white hiddenMenu' onClick={showMenu} id='icon-hamburguer' aria-label='icon-hamburguer-menu'>
                         <i className='bi bi-list fs-1'></i>
                     </button>
                     <ul className='navbar-nav menu container title' ref={menuNav}>
-                        <Link
-                            className={`nav-link me-3 mb-4 mt-2 p-2 fs-4 text-white ${activeLink === 'Home' ? 'bg-success' : ''}`}
-                            to='/' onClick={() => handleClick('Home')}>
-                            Inicio
-                        </Link>
-                        <Link
-                            onClick={() => handleClick('Blog')}
-                            className={`nav-link me-3 mb-4 mt-2 p-2 fs-4 text-white ${activeLink === 'Blog' ? 'bg-success' : ''}`}
-                            aria-current='page'
-                            to='/blogHelpMechanic'>
-                            Blog
-                        </Link>
-                        <Link
-                            onClick={() => handleClick('Nosotros')}
-                            className={`nav-link me-3 mb-4 mt-2 p-2 fs-4 text-white ${activeLink === 'Nosotros' ? 'bg-success' : ''}`}
-                            aria-current='page'
-                            to='/nosotros'>
-                            Nosotros
-                        </Link>
-                        <Link
-                            onClick={() => handleClick('Calidad')}
-                            className={`nav-link me-3 mb-4 mt-2 p-2 fs-4 text-white ${activeLink === 'Calidad' ? 'bg-success' : ''}`}
-                            to='/calidad'>
-                            Calidad
-                        </Link>
+                        <li onClick={() => handleClick('Home')} className={`me-3 mb-4 mt-2 p-2 ${activeLink === 'Home' ? 'bg-success' : ''}`}>
+                            <Link aria-current='page'
+                                className='nav-link fs-4 text-white' to='/'>
+                                Inicio
+                            </Link>
+                        </li>
+                        <li onClick={() => handleClick('Blog')} className={`me-3 mb-4 mt-2 p-2 ${activeLink === 'Blog' ? 'bg-success' : ''}`}>
+                            <Link
+                                className='nav-link fs-4 text-white' aria-current='page'
+                                to='/blogHelpMechanic'>
+                                Blog
+                            </Link>
+                        </li>
+                        <li onClick={() => handleClick('Nosotros')} className={`me-3 mb-4 mt-2 p-2 ${activeLink === 'Nosotros' ? 'bg-success' : ''}`}>
+                            <Link
+                                className='nav-link fs-4 text-white' aria-current='page'
+                                to='/nosotros'>
+                                Nosotros
+                            </Link>
+                        </li>
+                        <li onClick={() => handleClick('Calidad')} className={`me-3 mb-4 mt-2 p-2 ${activeLink === 'Calidad' ? 'bg-success' : ''}`}>
+                            <Link aria-current='page'
+                                className='nav-link fs-4 text-white' to='/calidad'>
+                                Calidad
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
